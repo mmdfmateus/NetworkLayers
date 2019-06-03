035136e0888f27023583afa516b6cda6a1ae65ce
Mateus Mourao <mmdfmateus@gmail.com>
2019-06-03 02:38:40 -0300

    changed everything in redes

--- camada-rede/main.py
+++ camada-rede/main.py
@@ -1,6 +1,36 @@
-hosts = open("hosts.txt", "r")
+import os
 
-print("   NAME        IP")
-print("-----------------")
-for line in hosts:
-    print (line.split(",")[0] + "     " + line.split(",")[1])
+messageReceived = 'CLIENT4-CLIENT3-mensagem.txt'
+segmentSent = 'CLIENT3-CLIENT1-segmento.txt'
+segmentReceived = 'CLIENT3-CLIENT1-segmento.txt'
+
+def getMessageLength(filename):
+    return os.stat(filename).st_size
+
+def getMessageSequence():
+    
+    return 1
+
+def getSegmentSentHeader(length, sequence):
+    header = "SEQUENCE: " + str(sequence) + "\nLENGTH: " + str(length)
+    return header
+
+def getMessageReceivedBody(filename): #thats wrong
+    file = open(filename, 'r')
+    for line in file:
+        if(line == "DATA"):
+            return line
+    return line
+
+def sendSegment(header, body):
+    file = open(segmentSent, 'w')
+    file.write(header)
+    file.write(body)
+    file.close()
+    return True
+
+length = getMessageLength(messageReceived)
+sequence = getMessageSequence()
+header = getSegmentSentHeader(length, sequence)
+
+print header
