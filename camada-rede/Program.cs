ec083726b29217a73d24239dc4896bf2d6889299
Mateus Mourao <mmdfmateus@gmail.com>
2019-06-22 18:13:51 -0300

    change hostsTable.json name

--- camada-rede/Program.cs
+++ camada-rede/Program.cs
@@ -5,7 +5,7 @@ namespace camada_rede
     public class Program
     {
         public static FilesService filesService = new FilesService();
-        public static string hostsPath = "hosts.json";
+        public static string hostsPath = "hostsTable.json";
 
         public static void Main(string[] args)
         {
